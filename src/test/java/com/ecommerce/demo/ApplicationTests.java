8b6d6bd8dfa90384313bece01bdc77e47e455ac8
alberto <alberto_cb17@hotmail.com>
2022-06-01 18:13:11 +0200

    first commit

---
+++ src/test/java/com/ecommerce/demo/ApplicationTests.java
@@ -0,0 +1,24 @@
+package com.ecommerce.demo;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertTrue(true);
+	}
+
+	@Test
+	void applicationStarts() {
+		assertDoesNotThrow(() -> {
+			Application.main(new String[] {});
+		});
+	}
+
+}
